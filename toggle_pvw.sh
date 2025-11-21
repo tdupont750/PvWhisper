8e9e940c11cc3ee62eb51bbc12f5251063c45d63
tdupont750 <1409297+tdupont750@users.noreply.github.com>
2025-11-20 22:58:27 -0800

    Updated toggle shell

--- toggle_pvw.sh
+++ toggle_pvw.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo -n 'v' > '/tmp/pvwhisper.fifo'
+
+FIFO_FILE='/tmp/pvwhisper.fifo'
+
+# If it doesn't exist or is not a FIFO, do nothing
+[[ -p "$FIFO_FILE" ]] || exit 0
+
+# Abort if write can't proceed
+timeout 1 bash -c "echo -n 'v' > '$FIFO_FILE'" >/dev/null 2>&1 || true
